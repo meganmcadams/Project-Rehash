ae06f3809f4496d66da5911ce23129c94f8cc54c
meganmcadams <megmcmuffin14@gmail.com>
2023-01-20 22:49:37 -0600

    initial setup of flask and some pages

---
+++ README.md
@@ -0,0 +1,1 @@
+Credit for set up of Flask: https://pythonhow.com/python-tutorial/flask/web-development-with-python-and-flask/
---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/join/')
+def join():
+    return render_template('join.html')
+
+@app.route('/officers/')
+def officers():
+    return render_template('officers.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
