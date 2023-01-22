8ef40b1d0907c96cb31434f7ce5439b3d9db3d4c
meganmcadams <megmcmuffin14@gmail.com>
2023-01-21 21:52:26 -0600

    trying to add core stats to char sheet

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect
+from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -18,6 +25,22 @@ def officers():
 @app.route('/character_sheet/')
 def character_sheet():
     return render_template('character_sheet.html')
+
+class Profile(db.Model):
+    # Id : Field which stores unique id for every row in
+    # database table.
+    # first_name: Used to store the first name if the user
+    # last_name: Used to store last name of the user
+    # Age: Used to store the age of the user
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=False, nullable=False)
+    last_name = db.Column(db.String(20), unique=False, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+    # repr method represents how one object of this datatable
+    # will look like
+    def __repr__(self):
+        return f"Name : {self.first_name}, Age: {self.age}"
 
 if __name__ == '__main__':
     app.run(debug=True)
